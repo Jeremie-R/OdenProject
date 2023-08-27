91cdaaf209f1ed49277344fe39f44458ad481600
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2023-08-27 20:50:28 +0200

    start of javascript

---
+++ EarlyJavascript/script.js
@@ -0,0 +1,18 @@
+console.log("helllooo");
+
+let message = 'alert example';
+alert(message);
+
+// execerice from https://www.theodinproject.com/lessons/foundations-fundamentals-part-1 //
+
+console.log(23+97);
+
+console.log((4+6+9)/77);
+
+let a = 10;
+console.log(a);
+
+console.log(9*a);
+
+let b = 7*a;
+console.log(b)
